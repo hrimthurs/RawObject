acd24819cf194eb8081659634440042a7310e14b
hrimthurs <negociant2007@mail.ru>
2022-09-29 02:11:40 +0300

    1.0.9: fix index module.exports

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 ﻿const ObjectToBuffer = require('./ObjectToBuffer.js')
 const ObjectFromBuffer = require('./ObjectFromBuffer.js')
 
-module.exports.toBuffer = (srcObj, inOptions) => ObjectToBuffer(srcObj, inOptions)
-module.exports.fromBuffer = (buffer) => ObjectFromBuffer(buffer)
+module.exports = {
+    toBuffer: (srcObj, inOptions) => ObjectToBuffer(srcObj, inOptions),
+    fromBuffer: (buffer) => ObjectFromBuffer(buffer)
+}
